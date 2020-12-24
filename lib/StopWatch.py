185a28392c01252f35faf232269ef18b44eb265b
Agajan Torayev <torayeff@gmail.com>
2020-12-24 13:17:51 +0000

    new methods

--- lib/StopWatch.py
+++ lib/StopWatch.py
@@ -7,15 +7,23 @@ class StopWatch:
         self.elapsed_milliseconds = 0
 
     def start(self):
+        self.elapsed_milliseconds = 0
         self.start_time = time.time()
 
     def stop(self):
-        if self.start_time == -1:
-            raise Exception('StopWatch should be started first!')
-
-        self.elapsed_milliseconds = int((time.time() - self.start_time)*1000)
+        self.elapsed_milliseconds = int((time.time() - self.start_time) * 1000)
         self.start_time = -1
 
     def restart(self):
         self.elapsed_milliseconds = 0
         self.start_time = time.time()
+
+    def reset(self):
+        self.elapsed_milliseconds = 0
+        self.start_time = -1
+
+    def elapsed_time(self):
+        if self.start_time == -1:
+            return 0
+        else:
+            return int((time.time() - self.start_time) * 1000)
